7ebf46901f8d3f69ff9a3c68b0edfbe457b36462
hossam-khalaf <hossamhassan303089@gmail.com>
2020-11-16 19:32:16 +0200

    UI && button functionallity

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
-## Idea
+# birthday reminder with react
+  following along with John smilga => Coding Addict
+### Idea
 
 [https://uidesigndaily.com/](https://uidesigndaily.com/posts/sketch-birthdays-list-card-widget-day-1042)
--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React, { useState } from 'react';
 import data from './data';
 import List from './List';
 function App() {
+  const [people, setPeople] = useState(data);
+
   return (
     <main>
       <section className='container'>
-        <h3>0 birthdays today</h3>
-        <List />
-        <button onClick={() => console.log('you clicked me')}>Clear All</button>
+        <h3>{people.length} birthdays today</h3>
+        <List people={people} />
+        <button onClick={() => setPeople([])}>Clear All</button>
       </section>
     </main>
   );
